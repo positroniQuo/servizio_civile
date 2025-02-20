86d08f8d1b1a8d36a640e29cde1cec9ec2767a7a
Filippo Arceci <100956831+positroniQuo@users.noreply.github.com>
2025-02-20 11:20:30 +0100

    npgraph.R

--- script.R
+++ script.R
@@ -13,18 +13,17 @@ fonts()
 # importing and loading all fonts from windows. 
 # fonts() is just for checking
 
-specie <- c("P. italiae","C. brachydactyla","C. cornix","M. striata","C. monedula",
-           "F. coelebs","P. ochruros","C.carduelis","U. epops","T. troglodytes",
-           "P. major","S. atricapilla","C. caeruleus","P. ater","J. torquilla",
-           "P. palustris","T. alba","S. europaea","A. caudatus","H. rustica",
-           "D. urbicum","L. megarhynchos","D. minor","G. glandaria")
+specie <- c("P. italiae","C. brachydactyla","M. striata",
+            "F. coelebs","P. ochruros","C.carduelis","U. epops","T. troglodytes",
+            "P. major","S. atricapilla","C. caeruleus","P. ater","J. torquilla",
+            "P. palustris","T. alba","S. europaea","A. caudatus","L. megarhynchos","D. minor")
 
 # list of the species names
 
 dfr <- data.frame(
- nomi        = factor(specie,levels =specie),
- inizio  = c(3,4,3,5,4,4,4,4,4,4,3,5,2,3,4,4,3,4,3,3,3,5,4,4),
-   fine  = c(8,7,6,7,6,7,6,8,6,5,7,6,6,7,7,7,9,5,6,9,9,6,5,6))
+  nomi        = factor(specie,levels =specie),
+  inizio  = c(3,4,5,4,4,4,4,4,3,5,2,3,4,4,3,4,3,5,4),
+  fine  = c(8,7,7,7,6,8,6,5,7,6,6,7,7,7,9,5,6,6,5))
 
 # this dataframe contains the month in which nesting start and end months, in the same order used for the species names to make them match
 
@@ -34,11 +33,11 @@ sdfr <- melt(dfr, measure.vars = c("inizio", "fine"))
 
 labels <- c("Gennaio", "Febbraio", "Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre")
 
-# nobody uses numbers in place of months, so here are the names for later use as lables
+# nobody uses numbers in place of months, so here are the names for later use as labels
 
 png("plot_nidificazione.png", width = 600, height = 500)     
 
-# let's save it as a png, specifying desired dimentions 
+# let's save it as a png, specifying desired dimensions 
 
 ggplot(sdfr, aes(value, nomi)) +                                                                  # i want to plot sdfr, using the species names
   ggtitle("Periodi di nidificazione")+                                                            # title
@@ -46,7 +45,7 @@ ggplot(sdfr, aes(value, nomi)) +                                                
   geom_line(size = 1.5, colour="darkgreen",lineend = "square") +                                  # 2nd line, green filling
   xlab(NULL) + 
   ylab(NULL) +
-  theme(plot.title = element_text(size=18, face="bold",hjust = 0.3),                              # title size, style and position
+  theme(plot.title = element_text(size=18, face="bold",hjust = 0.5),                              # title size, style and position
         text=element_text(family="Times New Roman", size=16),                                     # font and size for everything else
         axis.text.x = element_text(color = "darkgreen"),                                          # i want x lables to be green
         axis.text.y = element_text(face="italic")) +                                              # i also want y lables to be italic
@@ -56,3 +55,4 @@ ggplot(sdfr, aes(value, nomi)) +                                                
 dev.off()
 
 # close and save, goodbye!
+ 
